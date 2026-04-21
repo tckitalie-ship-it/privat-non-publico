8f7063ee3856233b99db1940e8ee73ee3268dbc1
tckitalie-ship-it <tckitalie@gmail.com>
2026-04-22 01:49:21 +0200

    feat: aggiunto login dto e update config

--- apps/web/app/app.module.ts
+++ apps/web/app/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MembershipsModule } from './memberships/memberships.module';
+import { AssociationsModule } from './associations/associations.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [UsersModule, AuthModule, MembershipsModule, AssociationsModule],
 })
 export class AppModule {}
