198e9b11a273338ed368a28b9272f392c06d1c45
tckitalie-ship-it <tckitalie@gmail.com>
2026-05-12 00:45:55 +0200

    Fix api build for Railway

--- apps/api/src/memberships/memberships.service.ts
+++ apps/api/src/memberships/memberships.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { Role } from '@prisma/client';
 
 @Injectable()
 export class MembershipsService {
   constructor(private prisma: PrismaService) {}
 
-  async findAll(associationId: string) {
+  async findAll(user: any) {
     return this.prisma.membership.findMany({
       where: {
-        associationId,
+        associationId: user.associationId,
       },
       include: {
         user: {
@@ -24,6 +29,55 @@ export class MembershipsService {
           },
         },
       },
+    });
+  }
+
+  async me(user: any) {
+    return this.prisma.membership.findFirst({
+      where: {
+        userId: user.id,
+        associationId: user.associationId,
+      },
+      include: {
+        association: true,
+      },
+    });
+  }
+
+  async updateRole(id: string, role: Role, user: any) {
+    const membership = await this.prisma.membership.findUnique({
+      where: { id },
+    });
+
+    if (!membership) {
+      throw new NotFoundException('Membership not found');
+    }
+
+    if (membership.associationId !== user.associationId) {
+      throw new ForbiddenException();
+    }
+
+    return this.prisma.membership.update({
+      where: { id },
+      data: { role },
+    });
+  }
+
+  async remove(id: string, user: any) {
+    const membership = await this.prisma.membership.findUnique({
+      where: { id },
+    });
+
+    if (!membership) {
+      throw new NotFoundException('Membership not found');
+    }
+
+    if (membership.associationId !== user.associationId) {
+      throw new ForbiddenException();
+    }
+
+    return this.prisma.membership.delete({
+      where: { id },
     });
   }
 }
