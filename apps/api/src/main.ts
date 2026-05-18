dee78536526e28092d1bd6a8c9b8102ee789bbdb
tckitalie-ship-it <tckitalie@gmail.com>
2026-05-18 10:48:54 +0200

    fix render port binding

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
+      'http://localhost:3000',
       'http://localhost:3001',
+      'http://127.0.0.1:3000',
       'http://127.0.0.1:3001',
     ],
     credentials: true,
@@ -21,7 +23,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3001;
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
